2bb7adba7209a9fe201ac7f3265ca427425d7a72
Serge Sha <sergesha@mail.ru>
2018-08-19 20:51:06 +0200

    App State updated (+ router) + Logger added

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,20 +1,24 @@
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import { storeFreeze } from 'ngrx-store-freeze';
-
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
-
-import * as fromFeatureOne from '../features/feature-one/store/feature-one.reducer';
-import * as fromFeatureTwo from '../features/feature-two/store/feature-two.reducer';
+import * as fromRouter from '@ngrx/router-store';
 
 export interface State {
-  featureOne: fromFeatureOne.State;
-  featureTwo: fromFeatureTwo.State;
+    router: fromRouter.RouterReducerState;
 }
 
 export const reducers: ActionReducerMap<State> = {
-    featureOne: fromFeatureOne.reducer,
-    featureTwo: fromFeatureTwo.reducer
-  }
-;
+    router: fromRouter.routerReducer
+};
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [ storeFreeze ] : [];
+// console.log all actions
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+    return function(state: State, action: any): State {
+        console.log('state', state);
+        console.log('action', action);
+
+        return reducer(state, action);
+    };
+}
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [ logger, storeFreeze ] : [];
